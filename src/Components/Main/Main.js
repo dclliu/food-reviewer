75298955eb8bf1d8f34d92dc29af44f99fa9c301
Daniel Liu <danielliu@daniels-mbp-4.lan>
2020-05-25 13:36:43 -0400

    More css changes to main

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -7,8 +7,12 @@ class Main extends React.Component {
     render() {
         return (
             <div className="Main">
-                <h1> food-reviewer </h1>
-                <h1> main page </h1>
+                <div className = "text-container">
+                <div className = "text">
+                    <h1> food-reviewer </h1>
+                    <h1> main page </h1>
+                </div>
+                </div>
                 <BigSearchBar handleSearch = {() => {}}/>
             </div>
         );
