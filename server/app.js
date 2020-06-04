cd45e05cb34df17239dfffba6394674471359a47
Daniel Liu <danielliu@daniels-mbp-4.lan>
2020-06-03 20:04:36 -0400

    Directory changes

--- server/app.js
+++ server/app.js
@@ -10,6 +10,8 @@ var reviewRouter = require('./routes/review.routes');
 
 var app = express();
 
+const publicPath = path.join(__dirname,  'public');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -18,13 +20,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/*', function (req, res) {
-   res.sendFile(path.join(__dirname, 'build', 'index.html'));
- });
+app.use(express.static(publicPath));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
